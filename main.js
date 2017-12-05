6926e5c29aaa09564468dcdbf0536b44ceab314c
zlatac <[idahosa1adaghe@hotmail.ca]>
2017-12-05 05:38:22 -0500

    update

--- main.js
+++ main.js
@@ -1,62 +1,53 @@
 var csv = require('csv');
 const fs = require('fs');
 const moment = require('moment');
-// change file reading to async when your done testing
 getPrices = new Promise (function(resolve,reject){
     var basket = [];
-    var data = fs.readFileSync('data/prices.csv').toString();
-    csv.parse(data,{columns:true},function(err,output){
-        if(safe(output)){               
-            output.forEach(function(item){
-                var list = {}
-                var date_refine = moment(item.DATE,'YYYY-MM-DD').unix();
-                item.unix = date_refine;
-                //list[date_refine] = item;
-                basket.push(item);
-            });
-            
-            basket.sort(function(a,b){
-                //sorts the price data from oldest date to latest date using UNIX timestamp
-            return a.unix - b.unix;
-            });
-            //console.log(basket);
-            resolve(basket);
-            //return basket;
-        }else{
-            return 'i dont know man';
-        }
-        return this.collector = 'ohhhhhhhhhhhhhhhhhhhhhhhhhh';
-    });
-    function safe(obj){
-        if(obj === null || obj === undefined || obj === ''){
-            return false;
-        }
-        return true;
-    };
-    //return this.collector;
+    var dataRead = fs.readFile('data/prices.csv',function(err,data){
+        csv.parse(data.toString(),{columns:true},function(err,output){
+            if(safe(output)){               
+                output.forEach(function(item){
+                    var list = {}
+                    var date_refine = moment(item.DATE,'YYYY-MM-DD').unix();
+                    item.unix = date_refine;
+                    //list[date_refine] = item;
+                    basket.push(item);
+                });
+                
+                basket.sort(function(a,b){
+                    //sorts the price data from oldest date to latest date using UNIX timestamp
+                return a.unix - b.unix;
+                });
+                //console.log(basket);
+                resolve(basket);
+                //return basket;
+            }else{
+                return 'i dont know man';
+            }
+        });
+    });    
+    
 });
 
-whyMe = function(){
-    // var f = moment().toString();
-    // return {'why':f};
-    // setTimeout(function(){
-    //     console.log('heyyyyyyyyyyyyy')
-    // },3000);
+function safe(obj){
+    if(obj === null || obj === undefined || obj === ''){
+        return false;
+    }
+    return true;
+};
 
-    var myFirstPromise = new Promise(function(resolve, reject){
-        // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
-        // In this example, we use setTimeout(...) to simulate async code. 
-        // In reality, you will probably be using something like XHR or an HTML5 API.
-        setTimeout(function(){
-          resolve(console.log('heyyyyyyyyyyyyy')); // Yay! Everything went well!
-        }, 3000);
+getFile = function(location){
+    return new Promise (function(resolve,reject){
+        var basket = [];
+        var dataRead = fs.readFile(location,function(err, data){
+            csv.parse(data.toString(),{columns:true},function(err,output){
+                if(safe(output)){               
+                    resolve(output);
+                    
+                }
+            });
+        });
     });
-
-    myFirstPromise.then(function(successMessage){
-        // successMessage is whatever we passed in the resolve(...) function above.
-        // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
-        console.log("Yay! ");
-      });
 }
 
 // getPrices.then(function(data){
@@ -64,5 +55,4 @@ whyMe = function(){
 // });
 
 module.exports.getPrices = getPrices;
-
-//console.log(obj);
+module.exports.getFile = getFile;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var formidable = require('express-formidable');
 var main = require('./main.js');
 var app = express();
+app.use(formidable());
 
 app.get('/', function (req, res) {
- res.send('Hello World');
+ res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/baby', function (req, res) {
@@ -11,8 +13,45 @@ app.get('/baby', function (req, res) {
     main.getPrices.then(function(data){
         res.send(data);
     });
-    //console.log(api);
-    
 });
+
+app.post('/form', function(req,res){
+    //console.log(req.files);
+    var output = {};
+    //form file input name is crucial for grabing file object
+    var path = req.files.boobs.path;
+    main.getFile(path)
+    .then(function(data){
+        output.file = data;
+        return main.getPrices;
+        
+    })
+    .then(function(data){
+        output.prices = data;
+        output.etf_price = [];
+        //get the most recent price and it will be the last index of the array because it has been sorted by time
+        var newPrice = output.prices[data.length - 1];
+        output.recent_date = output.prices[data.length - 1].DATE;
+        output.file.forEach(function(item){
+            item.price = newPrice[item.name];
+            item.holding = Number(item.price) * Number(item.weight);
+        });
+        //get ETF prices for each time period using only the data from uploaded csv
+        for(var x in output.prices){
+            var total = 0;
+            var basket = {};
+            output.file.forEach(function(item){
+                total += Number(output.prices[x][item.name]) * Number(item.weight);
+            });
+            basket.DATE = output.prices[x].DATE;
+            basket.total = total;
+            output.etf_price.push(basket);
+
+        }
+        res.send(output);
+    })
+    //res.send(req.files);
+});
+
 
 app.listen(3000);
